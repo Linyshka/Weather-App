86290e9daeff57029c0da2c73faf6f737b44e436
Alina Radyukevich <lina.radyukevich98@gmail.com>
2024-02-04 22:38:15 +0300

    feat: add today-forecast component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import SideBar from "./conponents/SideBar";
 import TenDaysForecast from './conponents/TenDaysForecast';
+import TodayForecast from './conponents/TodayForecast';
 import WeatherProperties from './conponents/WeatherProperties';
 
 function App() {
   return (
     <>
+      <TodayForecast />
+      <hr />
       <TenDaysForecast />
       <hr />
       <WeatherProperties />
---
+++ src/conponents/TodayForecast/TodayForecast.tsx
@@ -0,0 +1,12 @@
+function TodayForecast() {
+  return (
+    <>
+      <h1>Minsk</h1>
+      <h2>18&deg;</h2>
+      <p>Rainy</p>
+      <p>H:29&deg; L:15&deg;</p>
+    </>
+  );
+}
+
+export default TodayForecast;
