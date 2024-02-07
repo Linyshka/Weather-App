1631c192df39834f44d4b944a0203f152b221ab2
Alina Radyukevich <lina.radyukevich98@gmail.com>
2024-02-07 05:51:51 +0300

    feat: add background video

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import SideBar from "@/components/SideBar";
 import MainPage from './components/MainPage';
+import cloudly from './assets/videos/cloudly.mp4';
 import "./App.scss";
 
 function App() {
   return (
     <main className="main">
+      <video autoPlay muted loop className="background">
+        <source src={cloudly} type="video/mp4" />
+      </video>
       <MainPage />
       <SideBar />
     </main>
