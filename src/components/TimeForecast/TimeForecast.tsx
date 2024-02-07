aa99e90542d8ecadd3467b2e7d98fc54d57279d5
Alina Radyukevich <lina.radyukevich98@gmail.com>
2024-02-07 04:40:12 +0300

    feat: add styles for TimeForecast

--- src/components/TimeForecast/TimeForecast.tsx
+++ src/components/TimeForecast/TimeForecast.tsx
@@ -1,12 +1,16 @@
 import { timeForecastArray } from '@/data';
 import TimeItem from '../TimeItem';
 
+import style from "./TimeForecast.module.scss";
+
 function TimeForecast() {
   return (
-    <>
+    <div className={style.timeForecast}>
       <h2>Cloudy conditions from 1AM-9AM, with showers expected at 9AM.</h2>
-      {timeForecastArray.map((item, index) => <TimeItem key={index} item={item}/>)}
-    </>
+      <div className={style.timeBlock}>
+        {timeForecastArray.map((item, index) => <TimeItem key={index} item={item}/>)}
+      </div>
+    </div>
   )
 }
 
