18bcf7650cf21a7f6c33a466552f7c1a7aee2ba3
Alina Radyukevich <lina.radyukevich98@gmail.com>
2024-02-09 04:05:59 +0300

    feat: implement search by city name

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,12 +1,32 @@
-import LocationWidget from '../LocationWidget';
-import { locationsArray } from './constants';
+import { useState } from "react";
+import LocationWidget from "../LocationWidget";
+import { locationsArray } from "./constants";
 import styles from "./SideBar.module.scss";
 
 function SideBar() {
+  const [locations, setLocations] = useState(locationsArray);
+
+  const searchLocation = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchLocations = locationsArray.filter(
+      (item) =>
+        item.city.toLowerCase().search(e.target.value.toLowerCase()) !== -1
+    );
+    setLocations(searchLocations);
+  };
+
   return (
     <div className={styles.menu}>
-      <input type='text' placeholder='Search for a city or an airport' />
-      {locationsArray.map((city, index) => <LocationWidget key={index} location={city} />)}
+      <div className={styles.search}>
+        <input
+          type='text'
+          placeholder='Search for a city or an airport'
+          onChange={(e) => searchLocation(e)}
+        />
+      </div>
+
+      {locations.map((city, index) => (
+        <LocationWidget key={index} location={city} />
+      ))}
     </div>
   );
 }
