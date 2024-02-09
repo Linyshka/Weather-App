f4d33683807ed5edd688a43712508247f0683a8f
Alina Radyukevich <lina.radyukevich98@gmail.com>
2024-02-09 03:23:54 +0300

    feat: add background video for locations

--- src/components/LocationWidget/LocationWidget.tsx
+++ src/components/LocationWidget/LocationWidget.tsx
@@ -4,26 +4,45 @@ import { useState, useEffect, useContext } from "react";
 import { fetchWeather } from "@/api";
 import { WeatherInterface } from "@/api/interfaces";
 import { weatherCodes } from "@/api/constants";
-import { WeatherContext } from '@/App';
+import { WeatherContext } from "@/App";
+
+import cloudly from "@/assets/videos/cloudly.mp4";
+import rainly from "@/assets/videos/rainly.mp4";
+import snowly from "@/assets/videos/snowly.mp4";
+import sunny from "@/assets/videos/sunny.mp4";
+import thunderstorm from "@/assets/videos/thunderstorm.mp4";
+import wind from "@/assets/videos/wind.mp4";
 
 function LocationWidget({ location }: LocationProps) {
   const [weatherData, setWeatherData] = useState<WeatherInterface>();
   const setNewData = useContext(WeatherContext).setNewData;
+  const [videoUrl, setVideoUrl] = useState("");
 
   useEffect(() => {
-    fetchWeather(location.latitude, location.longitude).then((data) =>
-      setWeatherData(data)
-    );
+    fetchWeather(location.latitude, location.longitude).then((data) => {
+      setWeatherData(data);
+      setBackgroundVideo(data.current.weatherCode);
+    });
   }, [location.latitude, location.longitude]);
 
   const changeMainWeather = () => {
     if (weatherData) {
       setNewData(weatherData);
     }
-  }
+  };
+
+  const setBackgroundVideo = (code: number) => {
+    if (code === 0) setVideoUrl(sunny);
+    else if (code <= 48) setVideoUrl(cloudly);
+    else if (code <= 67 || (code >= 80 && code <= 82)) setVideoUrl(rainly);
+    else if (code <= 77 || code === 85 || code === 86) setVideoUrl(snowly);
+    else if (code === 95) setVideoUrl(thunderstorm);
+    else setVideoUrl(wind);
+  };
 
   return (
     <div className={styles.location} onClick={changeMainWeather}>
+      <video src={videoUrl} autoPlay muted loop></video>
       <div className={styles.place}>
         {location.isMyLocation ? (
           <div>
@@ -49,8 +68,9 @@ function LocationWidget({ location }: LocationProps) {
           {Math.floor(weatherData?.current.temperature2m as number)}&deg;
         </span>
         <p>
-          H: {Math.floor(weatherData?.daily.temperature2mMax[0] as number)}&deg;
-          L: {Math.floor(weatherData?.daily.temperature2mMin[0] as number)}&deg;
+          H: {Math.floor(weatherData?.daily.temperature2mMax[0] as number)}
+          &deg; L:{" "}
+          {Math.floor(weatherData?.daily.temperature2mMin[0] as number)}&deg;
         </p>
       </div>
     </div>
